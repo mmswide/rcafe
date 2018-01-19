9622aa77e8c1e7ad0f242f1d50a47a6bd80e44fc
mac <mac@macs-Mac.local>
2018-01-19 14:44:50 -0500

    generate bulletin using scaffold

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Rcafe2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Pacific Time (US & Canada)' # UTC -08:00 - rails time:zones:all
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
